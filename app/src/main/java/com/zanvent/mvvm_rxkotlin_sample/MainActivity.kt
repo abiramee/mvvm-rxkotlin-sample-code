21b31a021da215dfc51929df256fe0d857c8e2f9
Abir Hossain Amee <amee11315@gmail.com>
2020-02-29 23:52:05 +0600

    first commit

---
+++ app/src/main/java/com/zanvent/mvvm_rxkotlin_sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.zanvent.mvvm_rxkotlin_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
